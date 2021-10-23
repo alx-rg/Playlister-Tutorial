030340d3379c1d3cf255669c1374010421d9f522
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-10-22 19:22:29 -0500

    updated gitignore

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # acs1710-Playlister
+<h1>Welcome to Playlister</h1>
+
+It's a YouTube Video Playlist app, so that you can keep track of your favorites and share with your friends.
--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+# remember to: source env/bin/activate
+# to see the website : export FLASK_ENV=development; flask run
+
 from flask import Flask
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
    """Return homepage."""
-   return 'Hello, world!'
+   return render_template('home.html', msg='Flask is...')
 
 if __name__ == '__main__':
    app.run(debug=True)
 
-   
