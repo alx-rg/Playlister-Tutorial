fe99fac188909be52b714030ebfd451773619f3e
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-10-30 16:55:52 -0500

    Users can create playlists

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # remember to: source env/bin/activate
 # to see the website : export FLASK_ENV=development; flask run
+# http://localhost:5000
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask.templating import render_template
 from pymongo import MongoClient
 
@@ -10,6 +11,13 @@ db = client.Playlister
 playlists = db.playlists
 
 app = Flask(__name__)
+
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
 
 # @app.route('/')
 # def index():
@@ -30,3 +38,28 @@ def playlists_index():
    """Show all playlists."""
    return render_template('playlists_index.html', playlists=playlists.find())
 
+@app.route('/playlists/new')
+def playlists_new():
+   return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    #Submit a new playlist.
+    print(request.form.to_dict())
+    return redirect(url_for('playlists_index'))
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    #Submit a new playlist.
+    # Grab the video IDs and make a list out of them
+    video_ids = request.form.get('video_ids').split()
+    # call our helper function to create the list of links
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
