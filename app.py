ebfe4404e8676c9cdf2c323bfb06e7628f7a4e57
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-10-30 17:52:04 -0500

    Users can see single playlists

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-# remember to: source env/bin/activate
-# to see the website : export FLASK_ENV=development; flask run
-# http://localhost:5000
-
 from flask import Flask, render_template, request, redirect, url_for
 from flask.templating import render_template
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
-
 app = Flask(__name__)
-
+# @app.route('/')
+# def index():
+#     return render_template('home.html', msg='Flask is Cool!!')
+if __name__ == '__main__':
+    app.run(debug=True)
+# playlists = [ 
+#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+# ]
 def video_url_creator(id_lst):
     videos = []
     for vid_id in id_lst:
@@ -19,47 +23,28 @@ def video_url_creator(id_lst):
         videos.append(video)
     return videos
 
-# @app.route('/')
-# def index():
-#    """Return homepage."""
-#    return render_template('home.html', msg='Flask is pretty cool...')
-
-if __name__ == '__main__':
-   app.run(debug=True)
-
-# OUR MOCK ARRAY OF PROJECTS
-playlists = [
-   { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-   { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' },
-   { 'title': 'Hockey Memes', 'description': 'Bloopers of the matches!' },
-]
 @app.route('/')
 def playlists_index():
-   """Show all playlists."""
-   return render_template('playlists_index.html', playlists=playlists.find())
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 @app.route('/playlists/new')
 def playlists_new():
-   return render_template('playlists_new.html')
+    return render_template('playlists_new.html')
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
-    #Submit a new playlist.
-    print(request.form.to_dict())
-    return redirect(url_for('playlists_index'))
-
-@app.route('/playlists', methods=['POST'])
-def playlists_submit():
-    #Submit a new playlist.
-    # Grab the video IDs and make a list out of them
     video_ids = request.form.get('video_ids').split()
-    # call our helper function to create the list of links
     videos = video_url_creator(video_ids)
     playlist = {
         'title': request.form.get('title'),
-        'description': request.form.get('description'),
+        'description': request.form.get('description'), 
         'videos': videos,
         'video_ids': video_ids
     }
     playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index'))
+    return redirect(url_for('playlists_index')) 
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
