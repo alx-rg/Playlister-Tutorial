c4cf02f613cb988caef00498de793979a952e7b3
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-10-30 21:49:56 -0500

    Users can edit, update, and destroy playlists

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+# remember to: source env/bin/activate
+# to see the website : export FLASK_ENV=development; flask run
+# http://localhost:5000
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask.templating import render_template
 from pymongo import MongoClient
@@ -29,7 +33,7 @@ def playlists_index():
 
 @app.route('/playlists/new')
 def playlists_new():
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', title='New Playlist')
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -48,3 +52,30 @@ def playlists_submit():
 def playlists_show(playlist_id):
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    # Add the title parameter here
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    # create our updated playlist
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    # set the former playlist to the new one we just updated/edited
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    # take us back to the playlist's show page
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
