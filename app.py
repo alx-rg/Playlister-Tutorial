28e72934bbe1c2f1b7a6aa199fda042d7090502d
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-10-30 23:22:02 -0500

    Added tests

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # remember to: source env/bin/activate
 # to see the website : export FLASK_ENV=development; flask run
 # http://localhost:5000
+# Start DB with: brew services start mongodb-community@5.0 
+# Stop DB with: brew services stop mongodb-community@5.0
+
 
 from flask import Flask, render_template, request, redirect, url_for
 from flask.templating import render_template
@@ -14,26 +17,23 @@ app = Flask(__name__)
 # @app.route('/')
 # def index():
 #     return render_template('home.html', msg='Flask is Cool!!')
-if __name__ == '__main__':
-    app.run(debug=True)
+
 # playlists = [ 
 #     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
 #     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 # ]
-def video_url_creator(id_lst):
-    videos = []
-    for vid_id in id_lst:
-        video = 'https://youtube.com/embed/' + vid_id
-        videos.append(video)
-    return videos
 
 @app.route('/')
+def index():
+    return redirect('/playlists')
+
+@app.route('/playlists', methods=['GET'])
 def playlists_index():
     return render_template('playlists_index.html', playlists=playlists.find())
 
 @app.route('/playlists/new')
 def playlists_new():
-    return render_template('playlists_new.html', title='New Playlist')
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -41,12 +41,12 @@ def playlists_submit():
     videos = video_url_creator(video_ids)
     playlist = {
         'title': request.form.get('title'),
-        'description': request.form.get('description'), 
+        'description': request.form.get('description'),
         'videos': videos,
-        'video_ids': video_ids
+        'video_ids': video_ids,
     }
     playlists.insert_one(playlist)
-    return redirect(url_for('playlists_index')) 
+    return redirect(url_for('playlists_index'))
 
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
@@ -55,33 +55,36 @@ def playlists_show(playlist_id):
 
 @app.route('/playlists/<playlist_id>/edit')
 def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    # Add the title parameter here
     return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
 
 
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
-    """Submit an edited playlist."""
     video_ids = request.form.get('video_ids').split()
     videos = video_url_creator(video_ids)
-    # create our updated playlist
     updated_playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
         'videos': videos,
         'video_ids': video_ids
     }
-    # set the former playlist to the new one we just updated/edited
     playlists.update_one(
         {'_id': ObjectId(playlist_id)},
         {'$set': updated_playlist})
-    # take us back to the playlist's show page
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
-app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
 def playlists_delete(playlist_id):
-    """Delete one playlist."""
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
+
+def video_url_creator(id_lst):
+    videos = []
+    for vid_id in id_lst:
+        video = 'https://youtube.com/embed/' + vid_id
+        videos.append(video)
+    return videos
+
+if __name__ == '__main__':
+    app.run(debug=True)
