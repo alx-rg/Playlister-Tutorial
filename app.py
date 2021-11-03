91900cb69666ebbd7b674fa32ef33bc77b3234d0
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-11-03 11:46:34 -0500

    nav bar color

--- app.py
+++ app.py
@@ -105,9 +105,9 @@ def comments_new():
 
 
 @app.route("/playlists/comments/<comment_id>", methods=["POST"])
-def delete_comment(playlist_id, comment_id):
+def delete_comments(comment_id):
     comments.delete_one({"_id": ObjectId(comment_id)})
-    return redirect(url_for("playlists_show", playlist_id=playlist_id))
+    return redirect(url_for("playlists_show", playlist_id=request.form.get('playlist_id')))
 
 def video_url_creator(id_lst):
     videos = []
